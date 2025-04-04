73cdb4fa7c0d57474df22971d4bbdd4b67541fcf
Karim <karim.assimov@narxoz.kz>
2025-04-04 20:58:54 +0500

    Basic structure

--- src/proxy/GoodImage.java
+++ src/proxy/GoodImage.java
@@ -1,5 +1,21 @@
 package proxy;
 
 public class GoodImage  {
+    private String filename;
+    private String thumbnail;
+    private String highResImage;
 
+    public GoodImage(String filename) {
+        this.filename = filename;
+        loadThumbnail();
+    }
+    private void loadThumbnail() {
+        System.out.println("Uploading thumbnail for " + filename);
+        this.thumbnail = "Thumbnail: " + filename;
+    }
+
+    private void loadHighResImage() {
+        System.out.println("Loading high resolution image for " + filename);
+        this.highResImage = "High Resolution: " + filename;
+    }
 }
--- src/proxy/ProxyImage.java
+++ src/proxy/ProxyImage.java
@@ -2,6 +2,12 @@ package proxy;
 
 
 public class ProxyImage {
+    private GoodImage realImage;
+    private String filename;
+    private boolean isAgent;
 
-
+    public ProxyImage(String filename, boolean isAgent) {
+        this.filename = filename;
+        this.isAgent = isAgent;
+    }
 }
