cc75294dfa74ea765ca18ea0e0777a823b167862
Karim <karim.assimov@narxoz.kz>
2025-04-04 21:16:22 +0500

    Example ProxyImage

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import proxy.Image;
+import proxy.ProxyImage;
 
 public class Main {
     public static void main(String[] args) {
+        Image agentImage1 = new ProxyImage("beach_house.jpg", true);
+
+        Image userImage1 = new ProxyImage("beach_house.jpg", false);
+
+        System.out.println("Agent View: ");
+        agentImage1.display();
+        agentImage1.displayFullImage();
+
+        System.out.println();
+        System.out.println("User View: ");
+        userImage1.display();
+        userImage1.displayFullImage();
 
     }
 }
